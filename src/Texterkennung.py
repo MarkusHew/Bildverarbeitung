25c64fdec3d89b4651ba4fb2024eeb728e1f357e
RiaanGitHub <95358081+RiaanGitHub@users.noreply.github.com>
2024-04-25 12:41:52 +0200

    Update Texterkennung.py

--- src/Texterkennung.py
+++ src/Texterkennung.py
@@ -148,29 +148,71 @@ def boundingBox(img, Speicherpfad):
     return color_image
 
 
-def logo(img, pfad):  #Methode die Logo in Bild erkennt und Shopnamen zuruckgibt    
+#def logo(img, pfad):  #Methode die Logo in Bild erkennt und Shopnamen zuruckgibt    
+#    print("Aktuelles Verzeichnis:", pfad)
+#    shop_names={"Coop":"Logo_Coop.png","Volg":"Logo_Volg.png"}
+#    found_shop=None
+#    for key,value in shop_names.items():
+#        color_image=Bild_skalieren_und_Farbe(img, 400)
+#        template = cv2.imread(pfad+ '\\in\\'+value)
+#        w, h = template.shape[0], template.shape[1]
+#        res = cv2.matchTemplate(color_image, template, cv2.TM_CCOEFF_NORMED)
+#        threshold = .8
+#        loc = np.where(res >= threshold)
+#        if loc[0].size > 0:  # Wenn ein Ubereinstimmung gefunden wurde
+#            found_shop = key
+#            # Markieren des gefundenen Bereichs im Bild
+#            for pt in zip(*loc[::-1]):
+#                cv2.rectangle(color_image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+#            
+#            cv2.imshow('Ausschnitt in bild gefunden', color_image)
+#            cv2.imshow('template', template)
+#            cv2.waitKey(0)
+#            return True, found_shop     
+#          
+#    return False, None
+
+def logo(img, pfad):  # OS-sensitive Methode die Logo in Bild erkennt und Shopnamen zuruckgibt 
     print("Aktuelles Verzeichnis:", pfad)
-    shop_names={"Coop":"Logo_Coop.png","Volg":"Logo_Volg.png"}
-    found_shop=None
-    for key,value in shop_names.items():
-        color_image=Bild_skalieren_und_Farbe(img, 400)
-        template = cv2.imread(pfad+ '\\in\\'+value)
+    shop_names = {"Coop": "Logo_Coop.png", "Volg": "Logo_Volg.png"}
+    found_shop = None
+    
+    # Determine the path separator based on the current operating system
+    # (sep-assigning, only for illustrative purposes. ;-) )
+    if os.name == "posix":  # Linux or MacOS
+        sep = '/'
+    elif os.name == "nt":  # Windows
+        sep = '\\'
+    else:
+        sep = '/'
+        print("Unknown operating system. Assuming Linux path separator '/'.")
+
+    for key, value in shop_names.items():
+        color_image = Bild_skalieren_und_Farbe(img, 400)
+        template_path = os.path.join(pfad, 'in', value) # Pfaderweiterung per os.path.join() automatisch 
+                                                        # entspr. des erkannten Betriebssystems (OS)
+        template = cv2.imread(template_path)
+        
         w, h = template.shape[0], template.shape[1]
         res = cv2.matchTemplate(color_image, template, cv2.TM_CCOEFF_NORMED)
-        threshold = .8
+        threshold = 0.8
         loc = np.where(res >= threshold)
-        if loc[0].size > 0:  # Wenn ein Ubereinstimmung gefunden wurde
+        
+        if loc[0].size > 0:  # If a match is found
             found_shop = key
-            # Markieren des gefundenen Bereichs im Bild
+            
+            # Highlight the found area in the image
             for pt in zip(*loc[::-1]):
                 cv2.rectangle(color_image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
             
             cv2.imshow('Ausschnitt in bild gefunden', color_image)
             cv2.imshow('template', template)
             cv2.waitKey(0)
-            return True, found_shop     
-          
+            
+            return True, found_shop
+    
     return False, None
+
 
 
 # =============================================================================
